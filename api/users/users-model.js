50962a9907296638d1b740b71f9e187c06bf9aab
MurrayWarnock <murray.warnock@gmail.com>
2021-08-23 15:35:32 -0600

    updateUserById

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,3 +1,14 @@
 const db = require('../data/db-config');
+const { getById } = require('../auth/auth-model')
 
 async function getAllUsers() { return await db('users'); }
+
+async function updateUserById (id, user) {
+    await db('users').where('user_id', id).update(user);
+    return getById(id);
+}
+
+module.exports = {
+    getAllUsers,
+    updateUserById,
+};
